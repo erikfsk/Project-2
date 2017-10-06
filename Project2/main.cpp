659b7b732d2662a1c71465f813b12f5c45a50fea
Unknown <erikfsk@student.matnat.uio.no>
2017-10-06 14:27:32 +0200

    awesome :D
    almost done :)

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -3,113 +3,137 @@
 #include <cmath>
 #include <armadillo>
 #include <fstream>
+#include <stdlib.h>
 #include <boost/timer.hpp>
 
 using namespace std;
 using namespace arma;
 
-double func_f (double x_i);
-mat LU_solver(mat f_tilde,mat A);
-mat specific_solver(mat b, mat f_tilde,int n);
-mat general_solver(mat a,mat b,mat c, mat f_tilde,int n);
+
+bool test_jacobi();
+vec jacobi(mat a, int n);
+void jacobi_solver(mat &a,int n,int k,int l);
+double biggest_func(mat a,int n, int &k, int &l);
 int write_to_file(string datafil,mat f_tilde,int n,double h);
+
+
+
+
 
 int main (int argc, char *argv[])
 {
-    int n = atoi(argv[1]); double x_start = 0.0; double x_slutt = 1.0;
-    double h = (x_slutt-x_start)/(n+1); //x_i = i*h
 
-    mat a = ones<vec>(n);a[0] = 0;
-    mat b = (ones<vec>(n))*(-2);
-    mat c = ones<vec>(n);c[n-1] = 0;
-    mat f_tilde = zeros<vec>(n);
-
-    for (int i = 0; i<n;i++){
-        f_tilde[i] = -(pow(h,2))*func_f((i+1)*h);
+    if(!test_jacobi) {
+        cout << "error" << endl;
+        exit(0);
     }
 
-    //armadillo solves everything
-    mat A = zeros<mat>(n,n);
+    double rho_end = 5;
+    //int n = atoi(argv[1]);
+    int n = 60;
+    double h = (rho_end)/n;
+
+
+    //non interact
+    mat V = zeros<mat>(n,n);
     for (int i=0; i<n; i++) {
-        A(i,i)=b[i];
-        if (i!=n-1) A(i,i+1) = c[i];
-        if (i!=n-1) A(i+1,i) = a[i+1];
+        V(i,i)= (2/(h*h)) +(h*i*h*i);
+        if (i < n-1){
+          V(i+1,i) = -1/(h*h);
+        }
+        if (i > 0){
+          V(i-1,i) = -1/(h*h);
+        }
     }
-    vec ar = solve(A,f_tilde);
-    //solving done
+
+    /*interact
+    double w_r = 0.1;
+    mat V = zeros<mat>(n,n);
+    for (int i=0; i<n; i++) {
+        V(i,i)= (2/(h*h)) +w_r*w_r*(h*i*h*i) + (1/(h*i));
+        if (i < n-1){
+          V(i+1,i) = -1/(h*h);
+        }
+        if (i > 0){
+          V(i-1,i) = -1/(h*h);
+        }
+    }
+    */
+    cout << jacobi(V,n) << endl;
+
+    //cout << sort(eig_sym(V)) << endl;
+    // 3, 7, 11
+    // omega1 non interact
 
     boost::timer t_test;
     t_test.elapsed();
-
-    boost::timer t;
-    mat general_answer = general_solver(a,b,c,f_tilde,n);
-    cout << "Time usage general: " << t.elapsed() << endl;
-    write_to_file("General.dat",general_answer,n,h);
-
-    boost::timer t_;
-    mat specific_answer = specific_solver(b,f_tilde,n);
-    cout << "Time usage specific: " << t_.elapsed() << endl;
-    write_to_file("Specific.dat",specific_answer,n,h);
-
-    boost::timer t__;
-    mat LU_answer = LU_solver(f_tilde,A);
-    cout << "Time usage LU: " << t__.elapsed() << endl;
-    write_to_file("LU.dat",LU_answer,n,h);
-    return 0 ;
 }
 
-mat general_solver(mat a,mat b,mat c, mat f_tilde,int n)
+vec jacobi(mat a, int n)
 {
-    //forward substitution
-    for (int i = 1; i<n;i++){
-        double alpha = a[i]/b[i-1];
-        a[i] = a[i] - alpha*b[i-1];
-        b[i] = b[i] - alpha*c[i-1];
-        f_tilde[i] = f_tilde[i] - alpha*f_tilde[i-1];
+    int k;
+    int l;
+    double biggest = biggest_func(a,n,k,l);
+    while (abs(biggest)>0.001){
+        jacobi_solver(a,n,k,l);
+        biggest = biggest_func(a,n,k,l);
     }
-
-    //backward substitution
-    for (int i = n-2; i>-1;i--){
-        double c_ledd = c[i]/b[i+1];
-        c[i] = c[i] - c_ledd*b[i+1];
-        f_tilde[i] = f_tilde[i] - c_ledd*f_tilde[i+1];
-    }
-
-    //scaling
-    for (int i = 0; i<n;i++){
-        f_tilde[i] = f_tilde[i]/b[i];
-        b[i] = b[i]/b[i];
-    }
-
-    return f_tilde;
+    vec eigs = sort(a.diag());
+    vec smallEigs(3);
+    smallEigs(0) = eigs(0);
+    smallEigs(1) = eigs(1);
+    smallEigs(2) = eigs(2);
+    return smallEigs;
 }
 
-mat specific_solver(mat b, mat f_tilde,int n)
-{
-    //forward substitution
-    for (int i = 1; i<n;i++){
-        double alpha = 1/b[i-1];
-        b[i] = b[i] - alpha;
-        f_tilde[i] = f_tilde[i] - alpha*f_tilde[i-1];
+
+
+void jacobi_solver(mat &a,int n,int k,int l){
+    double tau = (a(l,l) - a(k,k))/(2*a(k,l));
+    double t_;
+    if ( tau > 0 ) {
+        t_ = 1.0/ (tau + sqrt(1.0 + tau*tau));
+    } else {
+        t_ = -1.0/ (-tau + sqrt(1.0 + tau*tau));
     }
-    //backward substitution
-    for (int i = n-2; i>-1;i--){
-        f_tilde[i] = f_tilde[i] - (1./b[i+1])*f_tilde[i+1];
+    //double t_ = -tau + sqrt(1+(tau*tau));
+    double c_ = 1/sqrt(1 + (t_*t_));//cos(theta)
+    double s_ = c_*t_;//sin(theta)
+    double a_kk = a(k,k);
+    double a_ll = a(l,l);
+    double a_kl = a(k,l);
+
+    a(k,k) = (a_kk*c_*c_) - (2*a_kl*c_*s_) + (a_ll*s_*s_);
+    a(l,l) = (a_ll*c_*c_) + (2*a_kl*c_*s_) + (a_kk*s_*s_);
+    a(k,l) = ((a_kk - a_ll)*s_*c_) + (a_kl*(c_*c_ - s_*s_));
+    a(l,k) = a(k,l);
+    for(int i = 0;i<n;i++){
+        if (i !=k && i!= l){
+            double a_il = a(i,l);
+            double a_ik = a(i,k);
+            a(i,k) = a_ik*(c_) - a_il*(s_);
+            a(i,l) = a_il*(c_) + a_ik*(s_);
+            a(k,i) = a(i,k);
+            a(l,i) = a(i,l);
+        }
     }
-    //scaling
-    for (int i = 0; i<n;i++){
-        f_tilde[i] = f_tilde[i]/b[i];
-    }
-    return f_tilde;
 }
 
-mat LU_solver(mat f_tilde, mat A){
-    mat L,U;
-    lu(L,U,A);
-    mat Y = solve(L,f_tilde);
-    mat V = solve(U,Y);
-    return V;
+double biggest_func(mat a,int n, int &k, int &l){
+    double biggest = 0;
+    for(int i = 0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i!=j){
+                if(abs(a(i,j)) > abs(biggest)){
+                    biggest = a(i,j);
+                    k = i; l = j;
+                }
+            }
+        }
+    }
+    return biggest;
 }
+
 
 int write_to_file(string datafil,mat f_tilde,int n,double h)
 {
@@ -126,7 +150,10 @@ int write_to_file(string datafil,mat f_tilde,int n,double h)
     return 0;
 }
 
-double func_f (double x_i)
-{
-  return 100*exp(-10*x_i);
+
+bool test_jacobi(){
+    //mat a;
+    //a = jacobi();
+    return true;
 }
+
